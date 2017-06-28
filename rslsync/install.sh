f8185e6376e892b56d1bbbcf7a60581d621c4f80
kenney <zql254@vip.qq.com>
2017-06-28 18:38:13 +0800

    add

--- rslsync/install.sh
+++ rslsync/install.sh
@@ -9,5 +9,5 @@ chmod a+x $KSROOT/bin/rslsync
 # add icon into softerware center
 dbus set softcenter_module_rslsync_install=1
 dbus set softcenter_module_rslsync_version=0.1
-dbus set softcenter_module_rslsync_description="同步工具"
+dbus set softcenter_module_rslsync_description="Resilio Sync 同步工具"
 rm -rf $KSROOT/install.sh
